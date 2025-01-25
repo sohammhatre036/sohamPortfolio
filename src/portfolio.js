4b4555d2365d1c7e34b2aa68faeb2a17ae4f3cf9
Soham Mhatre <131856125+sohammhatre036@users.noreply.github.com>
2025-01-25 20:46:18 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -217,61 +217,40 @@ const bigProjects = {
 // Include certificates, talks etc
 
 const achievementSection = {
-  title: emoji("Achievements And Certifications 🏆 "),
-  subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+  title: emoji("Achievements And Certifications 🏆"),
+  subtitle: "Achievements, Certifications, Award Letters, and Other Cool Stuff!",
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
-      subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
-      footerLink: [
-        {
-          name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-        },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
-        }
-      ]
+      title: "SQL and Relational Databases",
+      subtitle: "Completed certification from Cognitive Classes.",
+      image: require("./assets/images/sql.jpg"), // Replace with an appropriate image
+      imageAlt: "SQL Certification Logo",
+      footerLink: []
     },
+    // {
+    //   title: "Data Visualization with Power BI",
+    //   subtitle: "Certification course by Great Learning.",
+    //   image: require("./assets/images/powerBICertificationLogo.webp"), // Replace with an appropriate image
+    //   imageAlt: "Power BI Certification Logo",
+    //   footerLink: []
+    // },
     {
-      title: "Google Assistant Action",
-      subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
-      imageAlt: "Google Assistant Action Logo",
-      footerLink: [
-        {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
-        }
-      ]
+      title: "Big Data Computing",
+      subtitle: "Completed NPTEL online course.",
+      image: require("./assets/images/nptel.jpg"), // Replace with an appropriate image
+      imageAlt: "Big Data Certification Logo",
+      footerLink: []
     },
-
-    {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      imageAlt: "PWA Logo",
-      footerLink: [
-        {name: "Certification", url: ""},
-        {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
-        }
-      ]
-    }
+    // {
+    //   title: "Python for Data Analysis",
+    //   subtitle: "Certification course by Great Learning.",
+    //   image: require("./assets/images/pythonCertificationLogo.webp"), // Replace with an appropriate image
+    //   imageAlt: "Python Certification Logo",
+    //   footerLink: []
+    // }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section
 };
 
 // Blogs Section
