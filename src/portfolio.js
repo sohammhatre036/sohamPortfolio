be4c68d2cb54dac835c8f08eea2f10e3cc27ddc4
Soham Mhatre <131856125+sohammhatre036@users.noreply.github.com>
2025-01-25 19:34:14 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -349,10 +349,10 @@ const contactInfo = {
 
 Twitter Section
 
-const twitterDetails = {
-  userName: "Soham_Mhatre_", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
-};
+// const twitterDetails = {
+//   userName: "Soham_Mhatre_", //Replace "twitter" with your twitter username without @
+//   display: true // Set true to display this section, defaults to false
+// };
 
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
@@ -372,7 +372,7 @@ export {
   // talkSection,
   // podcastSection,
   contactInfo,
-  twitterDetails,
+  // twitterDetails,
   isHireable,
   resumeSection
 };
